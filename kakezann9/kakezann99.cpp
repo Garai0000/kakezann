1b678d597f34345ba7beaddba557c0620e0f8336
garai <garai0226@gmail.com>
2023-03-04 13:00:47 +0900

    １～９９掛け算　追加

---
+++ kakezann9/kakezann99.cpp
@@ -0,0 +1,31 @@
+int main(void)
+{
+    int a;
+    int b;
+    int d[100][100];
+
+    for (a = 0; a <= 99; a++) {
+        for (b = 0; b <= 99; b++) {
+            d[a][b] = a * b;
+        }
+    }
+    printf("  ");
+    for (a = 1; a <= 99; a++) {
+        printf("%5d", a);
+    }
+    printf("\n");
+    for (b = 1; b <= 99; b++) {
+        printf("\n");
+        printf("%d", b);
+
+        for (a = 1; a <= 99; a++) {
+
+            printf("%5d", d[a][b]);
+        }
+    }
+    return 0;
+    // exe単体起動した時のポーズ処理
+#ifdef NDEBUG
+    system("pause");
+#endif
+}
